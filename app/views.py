4072c2fa668a7a32ceac60d511b4a2c276551912
maneesh-manuz <tictic940050@gmail.com>
2022-02-03 11:01:09 +0530

    basepage

--- app/views.py
+++ app/views.py
@@ -5,3 +5,5 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+def projects(request):
+    return render(request,'projects.html')
